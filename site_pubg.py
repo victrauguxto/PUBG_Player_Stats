0d7e99f16023170f2ac6be598fb0f6be1dae6856
victrauguxto <augusto.victor446@gmail.com>
2024-05-06 17:51:27 -0300

    funções especificadas

--- site_pubg.py
+++ site_pubg.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template, request, flash, redirect
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash, Markup, send_from_directory, escape
 import pandas as pd
 import os
+from werkzeug.utils import secure_filename
+
+upload_folder = os.path.join(os.getcwd(), 'upload')
 
 app = Flask(__name__)
 
@@ -10,10 +14,20 @@ app = Flask(__name__)
 def homepage():
     return render_template('homepage.html')
 
-
 @app.route("/articles")
 def articles():
-    return "Bem vindo à página de artigos"
+    return render_template('artigos.html')
+
+@app.route("/upload", methods=["GET", "POST"])
+def upload():
+    if request.method == "POST":
+        file = request.files['planilha_dados']
+        savepath = os.path.join(upload_folder, secure_filename(file.filename))
+        file.save(savepath)
+        return render_template('upload2.html')
+    else:
+        return render_template('upload.html')
+
 
 
 
