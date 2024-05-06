4baf32b4ca14824cf62e768e3104227761611166
victrauguxto <augusto.victor446@gmail.com>
2024-05-06 15:25:18 -0300

    retirando os erros

--- site_pubg.py
+++ site_pubg.py
@@ -16,21 +16,6 @@ def articles():
     return "Bem vindo à página de artigos"
 
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    if request.method == 'POST':
-        # Seu código de processamento de upload aqui
-        return 'Arquivo enviado com sucesso'
-    else:
-        # Se necessário, tratar outros métodos HTTP
-        return 'Método não permitido'
-
-
-    pd.readexcel(file)
-    # Por exemplo, gerar um gráfico a partir dos dados
-    # Aqui estamos apenas exibindo os primeiros registros do DataFrame
-    return df.head().to_html()
-
 
 if __name__ == "__main__":
     app.run(debug=True)
