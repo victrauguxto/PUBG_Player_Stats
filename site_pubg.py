58c89e8e9b77286895b091515996566ab062eb22
victrauguxto <augusto.victor446@gmail.com>
2024-05-06 21:37:14 -0300

    implementei login e senha

--- site_pubg.py
+++ site_pubg.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 import pandas as pd
 import os
 from werkzeug.utils import secure_filename
@@ -23,11 +23,24 @@ def upload():
         file = request.files['planilha_dados']
         savepath = os.path.join(upload_folder, secure_filename(file.filename))
         file.save(savepath)
-        return render_template('upload2.html')
+        files = os.listdir(upload_folder)
+        return render_template('upload2.html', files=files)
     else:
         return render_template('upload.html')
 
+@app.route('/home_logado')
+def home_logado():
+    return render_template("homepage_logado.html")
 
+@app.route('/login', methods=['POST'])
+def login():
+    nome = request.form.get('username')
+    senha = request.form.get('password')
+
+    if nome == "admin" and senha == "123456":
+        return render_template('homepage_logado.html', username = nome)
+    else:
+        return "Usuário Não Encontrado"
 
 
 if __name__ == "__main__":
